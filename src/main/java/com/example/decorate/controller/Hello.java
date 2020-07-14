dcd30086bdab66d51d56aa2ba5e79b124efb23c0
TOM <matetothuk@gamil.com>
2020-07-14 20:06:31 +0200

    hello controller implemented

---
+++ src/main/java/com/example/decorate/controller/Hello.java
@@ -0,0 +1,19 @@
+package com.example.decorate.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/hello")
+public class Hello {
+
+    @GetMapping
+    public ResponseEntity<String> getOrcFormData() {
+        return new ResponseEntity<>("Hello world!", OK);
+    }
+
+}
