02b755b98857074ea0dc2eb0a4785db3ee83542d
Preseka Zsolt <zsolt.preseka@gmail.com>
2020-07-25 23:23:49 +0200

    database

---
+++ src/main/java/com/example/decorate/controller/CalculatorController.java
@@ -0,0 +1,27 @@
+package com.example.decorate.controller;
+
+import com.example.decorate.service.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+
+    private CalculatorService calculatorService;
+
+    @Autowired
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public ResponseEntity getResult() {
+        return new ResponseEntity(this.calculatorService.getOneToTwoGore(), HttpStatus.OK);
+    }
+}
