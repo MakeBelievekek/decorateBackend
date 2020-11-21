4930e4b9966e30cd22e288955e73dedcd7150135
Make <matetothuk@gmail.com>
2020-11-21 21:26:52 +0100

    routing goes to angular

--- src/main/java/com/example/decorate/config/SpringWebConfig.java
+++ src/main/java/com/example/decorate/config/SpringWebConfig.java
@@ -4,11 +4,32 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 public class SpringWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/public/index.html");
+                    }
+                });
+    }
 
     @Bean
     public MessageSource messageSource() {
--- src/main/java/com/example/decorate/config/TransactionFilter.java
+++ src/main/java/com/example/decorate/config/TransactionFilter.java
@@ -1,11 +1,10 @@
 package com.example.decorate.config;
 
 
-
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
