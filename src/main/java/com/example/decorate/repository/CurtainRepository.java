00bdb306fbaa6d667ba6217601a62421cd471f0e
Preseka Zsolt <zsolt.preseka@gmail.com>
2020-12-12 18:45:38 +0100

    valami

--- src/main/java/com/example/decorate/repository/CurtainRepository.java
+++ src/main/java/com/example/decorate/repository/CurtainRepository.java
@@ -10,10 +10,19 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CurtainRepository extends JpaRepository<Curtain, Long> {
+public interface CurtainRepository extends JpaRepository<Curtain, Long>{
     @Query("SELECT curtain FROM Curtain curtain ")
     List<Curtain> getAllCurtains();
 
     Optional<Curtain> findById(Long curtainId);
+/*
+    @Query("SELECT curtain FROM Curtain curtain, AttributeListItem  attributeListItem " +
+            "WHERE curtain.key.id = attributeListItem.key.id " +
+            "AND attributeListItem.attribute.description =:attribute")
+    List<Curtain> getCurtainsByAttributes(@Param("attribute") String attribute);
 
+    @Query("SELECT  curtain FROM Curtain curtain  " +
+            "WHERE curtain.id in " +
+            "(SELECT attr.key.id FROM AttributeListItem attr WHERE attr.attribute.description=:attribute )  ")
+    List<Curtain> getCurtainsByAttribute(@Param("attribute") String attribute);*/
 }
